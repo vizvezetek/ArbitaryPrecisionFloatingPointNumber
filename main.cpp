f6c8c5bec68ea367789eb537478212678ef47c88
Peter <konczpeter001@gmail.com>
2020-10-08 21:37:56 +0200

    drop_wawe test

--- main.cpp
+++ main.cpp
@@ -7,57 +7,31 @@
 
 using namespace std;
 
+float drop_wawe (float x1, float x2) { //  (0,0) glob_min: -1
+    return 
+        -(
+            1 + cos( 12*sqrt( pow(x1,2)+pow(x2,2) ) )
+        )
+        /
+        (
+            0.5* ( pow(x1,2)+pow(x2,2) ) +2
+        )
+    ;
+}
+
+Fpn FPN_drop_wawe (Fpn x1, Fpn x2) { //  (0,0) glob_min: -1
+    
+	Fpn part1 = Fpn("-1.0")*( Fpn("1.0") + Fpn::cos( Fpn("12.0")* Fpn::sqrt( (x1*x1)+(x2*x2) ) ) );
+	Fpn part2 = (Fpn("0.5")* ( (x1*x1)+(x2*x2) )  + Fpn("2.0"));
+	
+	return (part1/part2);
+}
+
+
 int main(int argc, char* argv[]){
 
-    if (argc == 2){
-        try
-		{
-            //char* -> double && double -> string -> Fpn
-            double d1 = (double)strtod(argv[1],NULL);
-            Fpn f1(std::to_string((double)d1),100);
-
-			if (d1>0) cout << d1 << ";" << f1 << ";sqrt(" << d1 << ");" << std::sqrt(d1) << ";" << Fpn::sqrt(f1) << ";" << endl;
-			// // // cout << d1 << ";" << f1 << ";pow(" << d1 << ");" << std::pow(d1) << ";" << Fpn::pow(f1) << ";" << endl;
-			cout << d1 << ";" << f1 << ";abs(" << d1 << ");" << std::abs(d1) << ";" << Fpn::abs(f1) << ";" << endl;
-			cout << d1 << ";" << f1 << ";round(" << d1 << ");" << std::round(d1) << ";" << Fpn::round(f1) << ";" << endl;
-			cout << d1 << ";" << f1 << ";sin(" << d1 << ");" << std::sin(d1) << ";" << Fpn::sin(f1) << ";" << endl;
-			cout << d1 << ";" << f1 << ";cos(" << d1 << ");" << std::cos(d1) << ";" << Fpn::cos(f1) << ";" << endl;
-		}
-		catch(std::string e)
-		{
-			std::cout << e;
-		}
-    }
-	else if (argc == 3){
-        try
-		{
-            //char* -> double && double -> string -> Fpn
-            double d1 = (double)strtod(argv[1],NULL);
-            double d2 = (double)strtod(argv[2],NULL);
-            Fpn f1(std::to_string((double)d1),100);
-            Fpn f2(std::to_string((double)d2),100);
-
-			cout << d1 << ";" << d2 << ";" << f1 << ";" << f2 << ";(" << d1 << "+" << d2 << ");" << (d1+d2) << ";" << (f1+f2) << ";" << endl;
-			cout << d1 << ";" << d2 << ";" << f1 << ";" << f2 << ";(" << d1 << "-" << d2 << ");" << (d1-d2) << ";" << (f1-f2) << ";" << endl;
-			cout << d1 << ";" << d2 << ";" << f1 << ";" << f2 << ";(" << d1 << "*" << d2 << ");" << (d1*d2) << ";" << (f1*f2) << ";" << endl;
-			cout << d1 << ";" << d2 << ";" << f1 << ";" << f2 << ";(" << d1 << "/" << d2 << ");" << (d1/d2) << ";" << (f1/f2) << ";" << endl;
-			cout << d1 << ";" << d2 << ";" << f1 << ";" << f2 << ";(" << d1 << "==" << d2 << ");" << (d1==d2) << ";" << (f1==f2) << ";" << endl;
-			cout << d1 << ";" << d2 << ";" << f1 << ";" << f2 << ";(" << d1 << "!=" << d2 << ");" << (d1!=d2) << ";" << (f1!=f2) << ";" << endl;
-			cout << d1 << ";" << d2 << ";" << f1 << ";" << f2 << ";(" << d1 << "<" << d2 << ");" << (d1<d2) << ";" << (f1<f2) << ";" << endl;
-			cout << d1 << ";" << d2 << ";" << f1 << ";" << f2 << ";(" << d1 << "<=" << d2 << ");" << (d1<=d2) << ";" << (f1<=f2) << ";" << endl;
-			cout << d1 << ";" << d2 << ";" << f1 << ";" << f2 << ";(" << d1 << ">" << d2 << ");" << (d1>d2) << ";" << (f1>f2) << ";" << endl;
-			cout << d1 << ";" << d2 << ";" << f1 << ";" << f2 << ";(" << d1 << ">=" << d2 << ");" << (d1>=d2) << ";" << (f1>=f2) << ";" << endl;
-		}
-		catch(std::string e)
-		{
-			std::cout << e;
-		}
-    }
-    
-
-	else{
-		std::cout << "Incorrect number of parameters!";
-    }
+	cout << drop_wawe(0.000011920998076675,0.000000000108775197) << endl;
+	cout << FPN_drop_wawe( Fpn("0.000011920998076675"),Fpn("0.000000000108775197") ) << endl;
 
     return 0;
 }
